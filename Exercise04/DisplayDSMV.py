f61f3a12c389daf3656c6e65ac738971550fbf56
LukasFun <81930036+LukasFun@users.noreply.github.com>
2022-06-21 15:45:11 +0200

    Update DisplayDSMV.py

--- Exercise04/DisplayDSMV.py
+++ Exercise04/DisplayDSMV.py
@@ -5,9 +5,10 @@
 # 
 # Lukas Freudenberg (lfreudenberg@uni-osnabrueck.de)
 # Philipp Rahe (prahe@uni-osnabrueck.de)
-# 23.05.2022, ver1.10
+# 21.06.2022, ver1.11
 # 
 # Changelog
+#   - 21.06.2022: Update to maintain compatibility with newer version of DSMVLib module
 #   - 23.05.2022: Update to maintain compatibility with newer version of Arduino program,
 #                 fixed a bug that caused the serial connection to not be monitored at the beginning,
 #                 fixed a bug that falsely caused the reading to start after a serial reconnect
